ee946b46ac7f8adb0ca28dee89bf6475cc16deee
Maris <maris.svirksts@gmail.com>
2020-10-29 10:49:10 +0200

    Create Staircase.scala

---
+++ src/main/scala/hackerrank/Staircase.scala
@@ -0,0 +1,20 @@
+package hackerrank
+
+object Solution {
+
+  // Complete the staircase function below.
+  def staircase(n: Int) {
+    for( i <- 1 to n ) {
+      print(" " * {n - i} )
+      println("#" * i)
+    }
+  }
+
+  def main(args: Array[String]) {
+    val stdin = scala.io.StdIn
+
+    val n = stdin.readLine.trim.toInt
+
+    staircase(n)
+  }
+}
