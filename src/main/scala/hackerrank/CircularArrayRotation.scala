5cef7213ea023dc557596f6772d1babf40bc7931
Maris Svirksts <maris.svirksts@gmail.com>
2021-01-27 07:58:21 +0200

    Daily commit.

--- src/main/scala/hackerrank/CircularArrayRotation.scala
+++ src/main/scala/hackerrank/CircularArrayRotation.scala
@@ -1,41 +1,29 @@
 package hackerrank
 
-import java.io._
+// https://www.hackerrank.com/challenges/circular-array-rotation/problem
 
 object CircularArrayRotation {
 
   def main(args: Array[String]): Unit = {
     val stdin = scala.io.StdIn
+    val nkq   = stdin.readLine().split(" ")
 
-    val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
-
-    val nkq = stdin.readLine().split(" ")
-
-    val n = nkq(0).trim.toInt
-
+    val _ = nkq(0).trim.toInt
     val k = nkq(1).trim.toInt
-
     val q = nkq(2).trim.toInt
-
     val a = stdin.readLine().split(" ").map(_.trim.toInt)
 
-    val queries = Array.ofDim[Int](q)
-
-    for (i <- 0 until q) {
-      val queriesItem = stdin.readLine().trim.toInt
-      queries(i) = queriesItem
-    }
+    val queries: IndexedSeq[Int] = for {
+      i <- 0 until q
+    } yield stdin.readLine().trim.toInt
 
     val result = circularArrayRotation(a, k, queries)
 
-    printWriter.println(result.mkString("\n"))
-
-    printWriter.close()
+    println(result.mkString("\n"))
   }
 
   // Complete the circularArrayRotation function below.
-  def circularArrayRotation(a: Array[Int], k: Int, queries: Array[Int]): Array[Int] = {
-    println("a: " + a.toList)
+  def circularArrayRotation(a: IndexedSeq[Int], k: Int, queries: IndexedSeq[Int]): IndexedSeq[Int] = {
     val rotatedElements: List[Int] = a.toList.indices.map(x => {
       val shift: Int = a.length - (k % a.length)
       val index: Int = (shift + x) % a.length
